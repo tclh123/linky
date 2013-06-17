c9d78d63c98b9c92b081e15da25cfc427b72bdf1
tclh123 <tclh123@gmail.com>
2013-06-18 01:26:46 +0800

    add sandbox.html to use eval(js template rendering), as Chrome Extension CSP.

--- scripts/script.js
+++ scripts/script.js
@@ -2,16 +2,54 @@ var $ = function(id) { return document.getElementById(id); };
 
 // main entry
 document.addEventListener('DOMContentLoaded', function() {
-    init();
+    // till sandbox can support template rending
+    $('sandbox').onload = function() {
+        init();
+    }
+});
+
+// on result from sandboxed frame:
+window.addEventListener('message', function(event) {
+    var html = event.data.result || "invalid result";
+
+    console.log(html);
+
+    $("container").innerHTML = html;
 });
 
 function init() {
     $("btn").addEventListener("click", function(){
         foo();
     }, false);
+
+    var context = {
+        sets: [
+            { name:"name1" }
+        ]
+    };
+
+    var message = {
+        command: 'render',
+        templateName: 'tpl_list',
+        context: context
+    };
+    $('sandbox').contentWindow.postMessage(message, '*');
+
 }
 
 function foo() {
-    $("container").innerHTML = "<p>test</p>";
-}
+//    $("container").innerHTML = "<p>test</p>";
 
+    var context = {
+        sets: [
+            { name:"name1" }
+        ]
+    };
+
+    var message = {
+        command: 'render',
+        templateName: 'tpl_list',
+        context: context
+    };
+    $('sandbox').contentWindow.postMessage(message, '*');
+}
