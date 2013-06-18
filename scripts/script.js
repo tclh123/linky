51d877b9c9eb60807dc43dae0dec23abb13716f6
tclh123 <tclh123@gmail.com>
2013-06-18 14:37:30 +0800

    add actions(set-put, tab-del)

--- scripts/script.js
+++ scripts/script.js
@@ -15,18 +15,94 @@ window.addEventListener('message', function(event) {
     console.log(html);
 
     $("container").innerHTML = html;
+
+    // 绑定事件
+    /// TODO: 不允许 inline javascript 很烦啊，有什么优美的解决方法没？
+    /// bind click events
+    /// load data from localStorage
+    var context = {
+        sets: []
+    };
+    for(var i = 0; i < localStorage.length; i++) {
+        var key = localStorage.key(i);
+        var set = JSON.parse(localStorage[key]);
+        context.sets.push(set);
+    }
+    for (var i =0; i<context.sets.length; i++) {
+        $(_formatActionId('set', 'put', context.sets[i].id)).addEventListener('click', function(e) {
+            doSetPut(e.target.getAttribute('data-setId'));
+        }, false);
+
+        for(var j=0; j<context.sets[i].tabs.length; j++) {
+            console.log(_formatActionId('tab', 'del', context.sets[i].id, j));
+            $(_formatActionId('tab', 'del', context.sets[i].id, j)).addEventListener('click', function(e) {
+                doTabDel(e.target.getAttribute('data-setId'), e.target.getAttribute('data-tabId'));
+            }, false);
+        }
+    }
 });
 
-function init() {
-    $("btn").addEventListener("click", function(){
-        foo();
-    }, false);
 
+// TODO: a lot
+
+function init() {
+
+    // Test Data TODO:
+//    var context = {
+//        sets: [
+//            {
+//                id: "1",
+//                name: "一个set",
+//                tabs: [
+//                    {
+//                        url: "http://www.tclh123.com"
+//                    },
+//                    {
+//                        url: "http://www.baidu.com"
+//                    }
+//                ]
+//            },
+//            {
+//                id: "2",
+//                name: "又一个set",
+//                tabs: [
+//                    {
+//                        url: "http://www.tclh123.com"
+//                    },
+//                    {
+//                        url: "http://www.baidu.com"
+//                    }
+//                ]
+//            }
+//        ]
+//    };
+
+    if(!localStorage["set$1"]) {
+        localStorage["set$1"] = JSON.stringify({
+            id: "1",
+            name: "一个set",
+            tabs: [
+                {
+                    url: "http://www.tclh123.com"
+                },
+                {
+                    url: "http://www.baidu.com"
+                }
+            ]
+        });
+    }
+    updateView();
+}
+function updateView() {
+    /// load data from localStorage
     var context = {
-        sets: [
-            { name:"name1" }
-        ]
+        sets: []
     };
+    for(var i = 0; i < localStorage.length; i++) {
+        var key = localStorage.key(i);
+        var set = JSON.parse(localStorage[key]);
+        context.sets.push(set);
+    }
 
     var message = {
         command: 'render',
@@ -34,22 +110,43 @@ function init() {
         context: context
     };
     $('sandbox').contentWindow.postMessage(message, '*');
-
 }
 
-function foo() {
-//    $("container").innerHTML = "<p>test</p>";
 
-    var context = {
-        sets: [
-            { name:"name1" }
-        ]
-    };
+////////// util
 
-    var message = {
-        command: 'render',
-        templateName: 'tpl_list',
-        context: context
-    };
-    $('sandbox').contentWindow.postMessage(message, '*');
+function _formatKey(type, id) {
+    return type+'$'+id;
+}
+
+function _formatActionId(type, action, setId, tabId) {
+    return type+'-'+action+'-'+setId+ (tabId!=undefined? ('-'+tabId):'');
+}
+
+////////// Controllers
+
+////////// Page Actions
+
+function doSetPut(setId) {
+    var key = _formatKey('set', setId);
+    if(localStorage[key]) {
+        var set = JSON.parse(localStorage[key]);
+        chrome.tabs.getSelected(null, function(tab) {
+            set.tabs.push({
+                url: tab.url
+            });
+            localStorage[key] = JSON.stringify(set);
+            updateView();
+        });
+    }
+}
+
+function doTabDel(setId, tabIndex) {
+    var key = _formatKey('set', setId);
+    if(localStorage[key]) {
+        var set = JSON.parse(localStorage[key]);
+        set.tabs.splice(tabIndex, 1);
+        localStorage[key] = JSON.stringify(set);
+        updateView();
+    }
 }
